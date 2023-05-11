8a2e6ec1718915a87b82fd43d1f88049030c6005
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-11 20:46:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 provider "azurerm" {
-  skip_provider_registration = "true"
   subscription_id = "b16c0a43-e015-4b35-9a35-870ec8198240"
   client_id       = "f338c7d6-c681-4043-a3e2-28ee416faf45"
   client_secret   = "zZ98Q~NF5Z4e9DEauajGAhV1iVlKsSpkgH7slcac"
