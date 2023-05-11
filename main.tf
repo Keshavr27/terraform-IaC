92445c79c8ad8439959cd1a3e177c2b423087228
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-11 20:43:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "azurerm" {
   skip_provider_registration = "true"
-  subscription_id = "dae40c06-8624-4d26-ad89-a09c96fc946d"
+  subscription_id = "b16c0a43-e015-4b35-9a35-870ec8198240"
   client_id       = "f338c7d6-c681-4043-a3e2-28ee416faf45"
   client_secret   = "zZ98Q~NF5Z4e9DEauajGAhV1iVlKsSpkgH7slcac"
   tenant_id       = "dae40c06-8624-4d26-ad89-a09c96fc946d"
