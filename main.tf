b0dc49a81fcee2380dabe4da8e669d354267f754
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-11 20:37:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   subscription_id = "dae40c06-8624-4d26-ad89-a09c96fc946d"
   client_id       = "f338c7d6-c681-4043-a3e2-28ee416faf45"
   client_secret   = "zZ98Q~NF5Z4e9DEauajGAhV1iVlKsSpkgH7slcac"
-  tenant_id       = "f8cdef31-a31e-4b4a-93e4-5f571e91255a"
+  tenant_id       = "dae40c06-8624-4d26-ad89-a09c96fc946d"
   features {}
 }
 resource "azurerm_resource_group" "network" {
