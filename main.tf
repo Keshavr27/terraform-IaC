159ba1027c48230f0d4ca8c2377905e5427c46b8
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-12 13:10:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -132,6 +132,11 @@ resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
+resource "local_file" "private_key" {
+  content         = tls_private_key.example_ssh.private_key_pem
+  filename        = "example.pem"
+  file_permission = "0600"
+}
 output "tls_private_key" { 
     value = tls_private_key.example_ssh.private_key_pem 
     sensitive = true
