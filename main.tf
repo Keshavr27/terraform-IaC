6fe3b43ae3c2917a835c6bd446528e025f47026d
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-12 12:19:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.55.0"
+#     }
+#   }
 
-# Create a resource group if it doesn't exist
+# }
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  subscription_id = "b16c0a43-e015-4b35-9a35-870ec8198240"
+  client_id       = "f338c7d6-c681-4043-a3e2-28ee416faf45"
+  client_secret   = "zZ98Q~NF5Z4e9DEauajGAhV1iVlKsSpkgH7slcac"
+  tenant_id       = "dae40c06-8624-4d26-ad89-a09c96fc946d"
+  features {}
+}
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "myResourceGroup"
     location = "eastus"
