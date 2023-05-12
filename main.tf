cf9b3ed20d82459ccd19406381fa8b6caf25da31
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2023-05-12 12:46:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     location              = "eastus"
     resource_group_name   = azurerm_resource_group.myterraformgroup.name
     network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Basic_A0"
+    size                  = "Standard_B1s"
 
     os_disk {
         name              = "myOsDisk"
